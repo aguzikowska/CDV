3d3088d2147ea683f2c47242a6eeb92dd6f06d03
aguzikowska <aguzikowska@edu.cdv.pl>
2019-04-01 12:43:29 +0200

    pierwszy_plik

---
+++ 1_postawy.py
@@ -0,0 +1,59 @@
+print ("CDV")
+print (2)
+
+'''
+dsdasd
+'''
+
+#potęgowanie
+potega= 2 ** 10
+print (potega)
+
+#pobieranie danych z klawiatury
+imie = input()
+#konkatenacja +
+print("Twoje imię podane z klawiatury:" + imie)
+
+nazwisko = input("podaj nazwisko")
+print("Twoje nazwisko:" + nazwisko)
+print()
+
+
+#Twoje imie: ...., Twoje nazwisko: ...
+
+
+
+
+imie = input()
+nazwisko = input()
+print("Twoje imie :" + imie + ", Twoje nazwisko:" + nazwisko )
+
+'''
+użytkownik podaje z klawiatury swoj wiek wyswietl dane w formacie: twoj wiek: ... lat
+'''
+
+print("podaj swoj wiek: ", end="")
+wiek = input()
+print(type(wiek)) #string
+print("Twoj wiek: ", wiek, " lat")
+
+wiek1 = 20
+print(type(wiek1)) #int
+print("twoj wiek;" + wiek1 + "lata")
+
+
+nazwisko = "Kowalski"
+pierwszyZnak = nazwisko[0]
+print(pierwszyZnak)
+
+dlugosc = len(nazwisko)
+print(dlugosc)
+
+ostatniZnak = nazwisko[len(naz) - 1]
+print(ostatniZnak)
+
+ostatniZnak = nazwisko[-1]
+print(ostatniZnak)
+
+#konwersja
+x = "5"
